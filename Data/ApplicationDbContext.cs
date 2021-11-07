634cbe224f0e0b90a26eb793dde2170ed1353fba
monogamen <andrey.rusak.01@mail.ru>
2021-11-07 15:45:20 +0300

    Add Models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CEPAggregator.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CEPAggregator.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<CEP> CEPs { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            if (!Database.EnsureCreated())
+            {
+                InitDB();
+            }
+        }
+
+        private void InitDB()
+        {
+            int i = 0;
         }
     }
 }
---
+++ Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace CEPAggregator.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public City City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+    }
+}
