8e40adb10fdcbde8a0c0828eb3b63fe7dcf49cbc
Valery Shavel <valshavel@gmai.com>
2021-11-14 17:00:37 +0300

    Init yandex maps

---
+++ wwwroot/js/map.js
@@ -0,0 +1,19 @@
+﻿
+const START_PARAMS = {
+    center: [53.902284, 27.561831],
+    zoom: 10
+};
+
+class Map {
+
+    constructor() {
+        ymaps.ready(this.init);
+    }
+
+    init() {
+        this.map = new ymaps.Map("map", START_PARAMS, {
+            searchControlProvider: 'yandex#search'
+        });
+    }
+
+}
