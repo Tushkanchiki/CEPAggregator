e979aca220d68adc082e6417d445000c235f65e7
monogamen <andrey.rusak.01@mail.ru>
2021-11-15 20:51:03 +0300

    temp

---
+++ Classes/Helpers/InfoParsers/BelarusbankInfoParser.cs
@@ -0,0 +1,18 @@
+﻿//using CEPAggregator.Interfaces;
+//using CEPAggregator.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace CEPAggregator.Classes.Helpers.InfoParsers
+//{
+//    public class BelarusbankInfoParser : Parser, IInfoParser
+//    {
+//        private const string Url = 
+//        public Dictionary<string, string> GetInfo(CEP cep)
+//        {
+
+//        }
+//    }
+//}
---
+++ Controllers/CepController.cs
@@ -0,0 +1,33 @@
+﻿using CEPAggregator.Classes;
+using CEPAggregator.Data;
+using CEPAggregator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEPAggregator.Controllers
+{
+    public class CepController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CepController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var cep = _dbContext.CEPs.FirstOrDefault(c => c.Id == id);
+            if (cep == null)
+                return NoContent();
+
+            return View(new Dictionary<string, string>() { {"aboba", "hi" }, {"aboba2", "hi2" },  });
+        }
+    }
+}
