8cca932552ad1e784a6a4a5de8003bb5fe4df9a7
Valery Shavel <valshavel@gmai.com>
2021-11-17 20:10:15 +0300

    Recommend by rates

---
+++ Classes/Helpers/HelperRateParsersRegistration.cs
@@ -0,0 +1,13 @@
+﻿using CEPAggregator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEPAggregator.Classes.Helpers
+{
+    public class HelperRateParsersRegistration
+    {
+        public IRateParser[] Parsers = { new BelarusbankRateParser(), new AgroRateParser(), new DabrabytRateParser() };
+    }
+}
